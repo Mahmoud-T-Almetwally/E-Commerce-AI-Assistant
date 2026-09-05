bd81806ddd46726e55f6e78da481308839f7376f
Mahmoud-T-Al-Metwally <mahmoudtariq@std.mans.edu.eg>
2026-09-05 21:49:46 +0300

    Fix: Added stricter URL parsing

--- routes/auth.py
+++ routes/auth.py
@@ -1,6 +1,7 @@
 from functools import wraps
-from flask import Blueprint, render_template, request, redirect, url_for, session, flash
+from urllib.parse import urlparse
 from werkzeug.security import check_password_hash
+from flask import Blueprint, render_template, request, redirect, url_for, session, flash
 
 from database.db_setup import SessionLocal
 from database.models import User, UserRole
@@ -55,8 +56,11 @@ def login():
 
                 flash('Logged in successfully.', 'success')
                 
-                next_page = request.args.get('next')
-                if not next_page or not next_page.startswith('/'):
+                if next_page:
+                    parsed_url = urlparse(next_page)
+                    if parsed_url.netloc != '' or parsed_url.scheme != '':
+                        next_page = url_for('admin.dashboard_home')
+                else:
                     next_page = url_for('admin.dashboard_home')
                     
                 return redirect(next_page)
