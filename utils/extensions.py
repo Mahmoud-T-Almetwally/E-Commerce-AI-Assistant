cf04e37910505538467b6311f1b408173c4215af
Mahmoud-T-Al-Metwally <mahmoudtariq@std.mans.edu.eg>
2026-09-13 03:36:30 +0300

    Fix: Added missing 'socketio' to 'utils/extensions.py'

--- utils/extensions.py
+++ utils/extensions.py
@@ -2,8 +2,11 @@
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 from flask_wtf.csrf import CSRFProtect
+from flask_socketio import SocketIO
 
 csrf = CSRFProtect()
 
 # Swap storage_uri to Redis in production (limits are per-process otherwise).
 limiter = Limiter(key_func=get_remote_address, default_limits=[], storage_uri="memory://")
+
+socketio = SocketIO(cors_allowed_origins="*")
